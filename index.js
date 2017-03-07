67204cc257ea8f9ba7b7999ad2094fa5dde27528
Chance <chance.eakin@gmail.com>
2017-03-06 22:23:12 -0600

    basic plane color additions

---
+++ index.js
@@ -0,0 +1,28 @@
+if (typeof AFRAME === 'undefined') {
+  throw new Error('Component attempted to register before AFRAME was available.');
+}
+
+AFRAME.registerComponent('squaretessel', {
+  schema: {
+    width: {default: 5},
+    height: {default: 5},
+    widthSegments: {default: 32},
+    heightSegments: {default: 32},
+    color: {type: 'string', default: '0x000000'}
+  },
+  init: function () {
+    var scene = this.el.object3D;
+    var data = this.data;
+    console.log(data);
+    /* populates the three.js layer with registered data or defaults from schema.*/
+
+    var geometry = new THREE.PlaneGeometry( data.width, data.height, data.widthSegments, data.heightSegments );
+    var material = new THREE.MeshBasicMaterial( {color: data.color, side: THREE.DoubleSide} );
+    var plane = new THREE.Mesh( geometry, material );
+    scene.add( plane );
+  },
+  remove: function () {
+    var scene = this.el.object3D;
+    scene.remove(scene.getObjectByName("squaretessel"));
+  }
+});
